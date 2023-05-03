65ff2471907e51e6317f106972707576c1569f04
Daniel Sierra <daniel.sierra@ellucian.com>
2023-05-03 08:35:54 -0500

    Add new files

---
+++ language_instructor.py
@@ -0,0 +1,14 @@
+# This is the main script for the Modern Greek language instructor application
+
+import argparse
+import os
+import sys
+
+
+def main():
+    # TODO: Implement the main function
+    pass
+
+
+if __name__ == '__main__':
+    main()
